f03b5ec0ec26cd86a6336ea2727a5be16add4c19
Liran Martfel <liran.martfel@gmail.com>
2026-01-28 22:49:15 +0200

    Add files via upload

---
+++ Tergol_3_if_num_prime.py
@@ -0,0 +1,15 @@
+number: int = int(input("enter a number: "))
+i: int = 2
+while True:
+    if number == 2:
+        print("the number is a prime number")
+        break
+    if number % 2 == 0 or number % i == 0 or number == 1 or number < 0:
+        print("the number isn't a prime number")
+        break
+    if number % i != 0:
+        i += 1
+        if number == i:
+            print ("the number is a prime number")
+            break
+
