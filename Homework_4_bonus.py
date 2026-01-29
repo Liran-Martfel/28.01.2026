e6f179bbb0425902ecda752c907c6bbceadfd781
Liran Martfel <liran.martfel@gmail.com>
2026-01-29 18:38:54 +0200

    Update Homework_4_bonus.py

--- Homework_4_bonus.py
+++ Homework_4_bonus.py
@@ -1,13 +1,12 @@
 x: int = 1
-i = 1
+i = 0
 while True:
     number = int(input("Enter a number: "))
     if number > x:
         x = number
         i += 1
-        if i == 4:
+        if i == 3:
             break
     else:
         i = 1
-        x = 1
-
+        x = number
